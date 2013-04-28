9d816492f776819cdc596ed94755980c12fe50c2
Janille Maragh <janillemaragh@gmail.com>
2013-04-28 15:27:26 -0400

    Takes out old control algorithm, changes to new control algorithm

--- lobsters.cpp
+++ lobsters.cpp
@@ -13,6 +13,8 @@ using namespace std;
 #include <vector>
 #include <sys/time.h>
 #include "Serial.h"
+#include <stdio.h>
+#include <math.h>
 
 // Needed for getopt / command line options processing
 #include <unistd.h>
@@ -34,35 +36,55 @@ double tic() {
 //stolen code here
 class PartyTime {
 
-  bool m_arduino; // send tag detections to serial port?
+  bool m_arduino; // send directions to serial port?
 
-  double m_dAzimuth;
-  double m_cAzimuth;
-  double m_dAltitude;
-  double m_cAltitude;
-  double m_tolerance;
-  double m_LeftThruster;
-  double m_RightThruster;
-  double m_TopThruster;
-  double m_dDistance;
-  double m_cDistance;
+  double m_pitch; //from Jackie
+  double m_roll; //from Jackie
+  double m_yaw; //from Jackie
 
-  //Serial m_serial;
+//  double m_dAzimuth;
+//  double m_cAzimuth;
+//  double m_dAltitude;
+//  double m_cAltitude;
+//  double m_tolerance;
+//  double m_LeftThruster;
+//  double m_RightThruster;
+//  double m_TopThruster;
+//  double m_dDistance;
+//  double m_cDistance;
+
+  double m_Xis; //from Lucille
+  double m_Xgoal; //obtained from path
+  double m_Yis; //from Lucille
+  double m_Ygoal; //obtained from path
+  double m_headingIs; //calculated here, referenced to magnetic north
+  double m_headingGoal; //calculated here
+  double m_path[]; //array of all the path coordinates
+
+
+
+  Serial m_serial;
 
 public:
 
   // default constructor
   PartyTime() :
     // default settings, most can be modified through command line options (see below)
-    m_arduino(false),
-    m_dAzimuth(10),
-    m_cAzimuth(3),
-    m_dAltitude(7),
-    m_cAltitude(2),
-    m_tolerance(10),
-    m_LeftThruster(0),
-    m_RightThruster(0),
-    m_TopThruster(0)
+	m_pitch(0),
+	m_roll(0),
+	m_yaw(0),
+
+	m_arduino(false),
+
+//	m_dAzimuth(0),
+//    m_cAzimuth(0),
+//    m_dAltitude(0),
+//    m_cAltitude(0),
+//    m_tolerance(0),
+    m_LeftThrust(0),
+    m_RightThrust(0),
+    m_TopThrust(0)
+
    {}
 
   void setup() {
@@ -76,31 +98,60 @@ public:
 
   void loop() {
     while (true) {
-    	//if we're too far left
-    	if (m_cAzimuth < (m_dAzimuth - m_tolerance)) {
-    		// turn right
-    		m_LeftThruster = 12;
+    	double headingErr;
+    	double depthErr;
+    	double headingCurr;
+    	double headingPrev;
+    	double headingGoal;
+    	//if Xcurr and Ycurr are within range o
+    	//then index of carrotCurr = next
+    	//else index of carrotCurr  = same
+    	headingCurr = path(ind);
+    	if (ind == 0) {
+    		headingPrev = path(0);
+    		else {
+    			headingPrev = path(ind - 1);
+    		}
     	}
-    	//if we're too far right
-    	else if (m_cAzimuth > (m_dAzimuth + m_tolerance)) {
-    		// turn left
-    		m_RightThruster = 12;
-    	}
-    	// if we're tipped too far downward
-    	if (m_cAltitude < (m_dAltitude - m_tolerance)) {
-    	    //turn upwards
-    		m_TopThruster = -12;
-    	}
-    	//if we're tipped too far upward
-    	else if (m_cAltitude > (m_dAltitude + m_tolerance)) {
-    	    // turn upwards
-    		m_TopThruster = 12;
-    	// if we're oriented correctly
-    	if (m_cAzimuth < (m_dAzimuth + m_tolerance) && m_cAzimuth > (m_dAzimuth - m_tolerance) && m_cAltitude < (m_dAltitude + m_tolerance) && m_cAltitude > (m_dAltitude - m_tolerance)) {
-    		//move forwards
-    		m_LeftThruster = 12;
-    		m_RightThruster = 12;
-    	}
+    	headingErr = headingGoal - headingCurr;
+    	headingGoal = 90 - atan((Ygoal - Yis)/(Xgoal - Xis));
+
+
+
+
+
+    	//PD control of left thruster
+    	m_LeftThrust = (m_k1 * headingErr) + (m_k2 * (headingCurr - headingPrev));
+    	//PD control of right thruster
+    	m_RightThrust = -(m_k1 * headingErr) - (m_k2 * (headingCurr - headingPrev));
+    	//PD control of depth
+    	m_TopThrust = (m_k3 * depthErr
+
+//    	//if we're too far left
+//    	if (m_cAzimuth < (m_dAzimuth - m_tolerance)) {
+//    		// turn right
+//    		m_LeftThruster = 12;
+//    	}
+//    	//if we're too far right
+//    	else if (m_cAzimuth > (m_dAzimuth + m_tolerance)) {
+//    		// turn left
+//    		m_RightThruster = 12;
+//    	}
+//    	// if we're tipped too far downward
+//    	if (m_cAltitude < (m_dAltitude - m_tolerance)) {
+//    	    //turn upwards
+//    		m_TopThruster = -12;
+//    	}
+//    	//if we're tipped too far upward
+//    	else if (m_cAltitude > (m_dAltitude + m_tolerance)) {
+//    	    // turn upwards
+//    		m_TopThruster = 12;
+//    	// if we're oriented correctly
+//    	if (m_cAzimuth < (m_dAzimuth + m_tolerance) && m_cAzimuth > (m_dAzimuth - m_tolerance) && m_cAltitude < (m_dAltitude + m_tolerance) && m_cAltitude > (m_dAltitude - m_tolerance)) {
+//    		//move forwards
+//    		m_LeftThruster = 12;
+//    		m_RightThruster = 12;
+//    	}
 
     	if (m_arduino) {
 			// here put information to be printed to serial port
@@ -119,7 +170,7 @@ public:
 
   int main() {
 	PartyTime party;
-	// party.setup();
+	party.setup();
 	party.loop();
   }
 

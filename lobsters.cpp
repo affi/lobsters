bc166292e8b04c94f79635aff14a71be5fda950e
Janille Maragh <janillemaragh@gmail.com>
2013-04-29 18:49:13 -0400

    Includes the lawnmower path to be followed by robot

--- lobsters.cpp
+++ lobsters.cpp
@@ -22,157 +22,129 @@ extern int optind;
 extern char *optarg;
 
 const string usage = "\n"
-  "Usage:\n"
-  //put usage text here
-  "\n";
+		"Usage:\n"
+		//put usage text here
+		"\n";
 
 // utility function to provide current system time
 double tic() {
-  struct timeval t;
-  gettimeofday(&t, NULL);
-  return ((double)t.tv_sec + ((double)t.tv_usec)/1000000.);
+	struct timeval t;
+	gettimeofday(&t, NULL);
+	return ((double) t.tv_sec + ((double) t.tv_usec) / 1000000.);
 }
 
 //stolen code here
 class PartyTime {
 
-  bool m_arduino; // send directions to serial port?
+	bool m_arduino; // send directions to serial port?
 
-  double m_pitch; //from Jackie
-  double m_roll; //from Jackie
-  double m_yaw; //from Jackie
+	double m_pitch; //from Jackie
+	double m_roll; //from Jackie
+	double m_yaw; //from Jackie
 
-//  double m_dAzimuth;
-//  double m_cAzimuth;
-//  double m_dAltitude;
-//  double m_cAltitude;
-//  double m_tolerance;
-//  double m_LeftThruster;
-//  double m_RightThruster;
-//  double m_TopThruster;
-//  double m_dDistance;
-//  double m_cDistance;
+	double m_Xis; //from Lucille
+	double m_Xgoal; //obtained from path
+	double m_Yis; //from Lucille
+	double m_Ygoal; //obtained from path
+	double m_headingIs; //calculated here, referenced to magnetic north
+	double m_headingGoal; //calculated here
+	double m_LeftThrust;
+	double m_RightThrust;
+	double m_TopThrust;
+	int ind;
+	double m_Xwidth;
+	double m_Ywidth;
+	double m_numCoords;
+	double m_path[][];
 
-  double m_Xis; //from Lucille
-  double m_Xgoal; //obtained from path
-  double m_Yis; //from Lucille
-  double m_Ygoal; //obtained from path
-  double m_headingIs; //calculated here, referenced to magnetic north
-  double m_headingGoal; //calculated here
-  double m_path[]; //array of all the path coordinates
-
-
-
-  Serial m_serial;
+	Serial m_serial;
 
 public:
 
-  // default constructor
-  PartyTime() :
-    // default settings, most can be modified through command line options (see below)
-	m_pitch(0),
-	m_roll(0),
-	m_yaw(0),
+	// default constructor
+	PartyTime() :
+			// default settings
+			m_pitch(0), m_roll(0), m_yaw(0),
 
-	m_arduino(false),
+			m_arduino(false),
 
-//	m_dAzimuth(0),
-//    m_cAzimuth(0),
-//    m_dAltitude(0),
-//    m_cAltitude(0),
-//    m_tolerance(0),
-    m_LeftThrust(0),
-    m_RightThrust(0),
-    m_TopThrust(0)
+			m_LeftThrust(0), m_RightThrust(0), m_TopThrust(0),
 
-   {}
+			ind(0),
 
-  void setup() {
-    // prepare serial port for communication with Arduino
-    if (m_arduino) {
-      m_serial.open("/dev/ttyACM0");
-    }
-  }
+			m_Xwidth(10), m_Ywidth(10), m_numCoords(121)
 
-// not stolen code here
+	{
+	}
 
-  void loop() {
-    while (true) {
-    	double headingErr;
-    	double depthErr;
-    	double headingCurr;
-    	double headingPrev;
-    	double headingGoal;
-    	//if Xcurr and Ycurr are within range o
-    	//then index of carrotCurr = next
-    	//else index of carrotCurr  = same
-    	headingCurr = path(ind);
-    	if (ind == 0) {
-    		headingPrev = path(0);
-    		else {
-    			headingPrev = path(ind - 1);
-    		}
-    	}
-    	headingErr = headingGoal - headingCurr;
-    	headingGoal = 90 - atan((Ygoal - Yis)/(Xgoal - Xis));
-
-
-
-
-
-    	//PD control of left thruster
-    	m_LeftThrust = (m_k1 * headingErr) + (m_k2 * (headingCurr - headingPrev));
-    	//PD control of right thruster
-    	m_RightThrust = -(m_k1 * headingErr) - (m_k2 * (headingCurr - headingPrev));
-    	//PD control of depth
-    	m_TopThrust = (m_k3 * depthErr
-
-//    	//if we're too far left
-//    	if (m_cAzimuth < (m_dAzimuth - m_tolerance)) {
-//    		// turn right
-//    		m_LeftThruster = 12;
-//    	}
-//    	//if we're too far right
-//    	else if (m_cAzimuth > (m_dAzimuth + m_tolerance)) {
-//    		// turn left
-//    		m_RightThruster = 12;
-//    	}
-//    	// if we're tipped too far downward
-//    	if (m_cAltitude < (m_dAltitude - m_tolerance)) {
-//    	    //turn upwards
-//    		m_TopThruster = -12;
-//    	}
-//    	//if we're tipped too far upward
-//    	else if (m_cAltitude > (m_dAltitude + m_tolerance)) {
-//    	    // turn upwards
-//    		m_TopThruster = 12;
-//    	// if we're oriented correctly
-//    	if (m_cAzimuth < (m_dAzimuth + m_tolerance) && m_cAzimuth > (m_dAzimuth - m_tolerance) && m_cAltitude < (m_dAltitude + m_tolerance) && m_cAltitude > (m_dAltitude - m_tolerance)) {
-//    		//move forwards
-//    		m_LeftThruster = 12;
-//    		m_RightThruster = 12;
-//    	}
-
-    	if (m_arduino) {
-			// here put information to be printed to serial port
-    		// m_serial.print(detections[0].id);
-			 // m_serial.print(",");
-			 // m_serial.print(translation(0));
-			 // m_serial.print(",");
-			 // m_serial.print(translation(1));
-			  //m_serial.print(",");
-			  //m_serial.print(translation(2));
-			  //m_serial.print("\n");
+	void setup() {
+		// prepare serial port for communication with Arduino
+		if (m_arduino) {
+			m_serial.open("/dev/ttyACM0");
+		}
+		// create the path
+		double m_path[110][2]; //each row is one x,y coordinate
+		int n, m;
+		// fill in the elements of the array
+		for (n = 0; n < m_numCoords; n++) {
+			m_path[n][1] = n / 11;
+			if (m_path[n][1] % 2 == 0) {
+				m_path[n][0] = n % 11;
+			} else {
+				m_path[n][0] = 10 - (n % 11);
 			}
 		}
-    }
-  }
+	}
 
-  int main() {
+	void loop() {
+		while (true) {
+			double depthErr;
+			double headingCurr;
+			double headingPrev;
+			double carrotPrev;
+
+			if (Xgoal - r) < Xis < (Xgoal + r) && (Ygoal - r) < Yis < (Ygoal + r) {
+				ind++;
+			}
+
+			// carrotCurr is our current target
+			carrotCurr = path(ind);
+			// carrotPrev is our last target
+			if (ind == 0) {
+				carrotPrev = path(0);
+			else {
+				carrotPrev = path(ind - 1);
+			}
+		}
+		// calculates heading relative to +y direction
+		double headingGoal = 90 - atan((Ygoal - Yis) / (Xgoal - Xis));
+		// heading error to be used for PD control
+		double headingErr = headingGoal - headingCurr;
+
+		//PD control of left thruster
+		m_LeftThrust = (m_k1 * headingErr) + (m_k2 * (headingIs - headingPrev));
+		//PD control of right thruster
+		m_RightThrust = -(m_k1 * headingErr)
+				- (m_k2 * (headingIs - headingPrev));
+		//PD control of depth
+		m_TopThrust = (m_k3 * depthErr) + (m_k4 * (depthIs - depthPrev));
+
+		if (m_arduino) {
+			// thruster commands printed to serial port to arduino here
+			m_serial.print(m_LeftThrust);
+			m_serial.print(",");
+			m_serial.print(m_RightThrust);
+			m_serial.print(",");
+			m_serial.print(m_TopThrust);
+			m_serial.print("\n");
+		}
+	}
+}
+}
+
+int main() {
 	PartyTime party;
 	party.setup();
 	party.loop();
-  }
-
-
+}
 
